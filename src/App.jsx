f07a41e0659e17f8fc3d2174908dc76f53942a2d
Anders Newgard <anne9707@colorado.edu>
2023-12-18 21:14:32 -0500

    continued commenting

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,13 @@ import { useLocation } from 'react-router-dom'
 import './App.css'
 
 function App() {
+  // useLocation() is a hook that returns the location object that represents the current URL. Used to determine which page is currently being viewed.
   const currentPage = useLocation().pathname;
 
   return (
     <div className="App">
       <Header >
+        {/* Nav passed as a property in header component*/}
       <Nav currentPage={currentPage} />
       </Header>
       <main>
